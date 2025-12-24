13ee24065fe2ff4985b977e16eedf4a5b615ebb4
sihu-dev <josihu0604@gmail.com>
2025-12-24 17:41:00 +0900

    chore: clean up bidflow structure - remove design-system/hephaitos, backup folder, duplicate lock files

--- apps/bidflow/.bidflow_backup/src/components/spreadsheet/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * BIDFLOW Spreadsheet Components
- */
-
-export { SpreadsheetView } from './SpreadsheetView';
-export { Toolbar } from './Toolbar';
-export { SidePanel } from './SidePanel';
-export type { Bid } from './SpreadsheetView';
--- apps/bidflow/.design-system/hephaitos/components-dashboard/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Dashboard 컴포넌트 모듈
- */
-
-export { MetricCard, MetricCardGrid, MiniMetricCard } from './MetricCard';
